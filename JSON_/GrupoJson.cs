4591e131fcad36986c26281c9ad51bc75e16a611
AbMonts <94406847+AbMonts@users.noreply.github.com>
2025-11-06 13:23:56 -0600

    Delete JSON_ directory

--- JSON_/GrupoJson.cs
+++
@@ -1,5 +0,0 @@
-﻿public class GrupoJson
-{
-    public string NOMBRE { get; set; }
-    public string DESCRIPCIÓN { get; set; }
-}
--- JSON_/SeccionJson.cs
+++
@@ -1,6 +0,0 @@
-﻿public class SeccionJson
-{
-    public string NOMBRE {  get; set; }
-    public string DESCRIPCIÓN { get; set; }
-    public string FILTRO { get; set; }
-}
